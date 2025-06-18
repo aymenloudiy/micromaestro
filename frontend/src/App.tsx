1de47710d7bce3cddd979d5364e98dcd7f466b7a
Nimuel <yiduolnemya@gmail.com>
2025-06-18 11:43:52 -0400

    feat: configure App.tsx with BrowserRouter and nested routes using Layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,18 @@ import Layout from "./components/Layout";
 import InventoryView from "./components/InventoryView";
 import ScenarioManager from "./components/ScenarioManager";
 import ActionHistoryView from "./components/ActionHistoryView";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<InventoryView />} />
-          <Route path="/scenarios" element={<ScenarioManager />} />
-          <Route path="/history" element={<ActionHistoryView />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="inventory" element={<InventoryView />} />
+          <Route path="scenarios" element={<ScenarioManager />} />
+          <Route path="history" element={<ActionHistoryView />} />
+          <Route path="*" element={<div>Page not found</div>} />
         </Route>
       </Routes>
     </BrowserRouter>
